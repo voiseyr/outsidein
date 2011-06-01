5b3cfece28c12894bbeb0f0939fe0b15258a802a
Richard Voisey <voiseyr15@hotmail.co.uk>
2011-06-01 18:54:39 +0100

    First cucumber commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outsidein::Application.config.secret_token = '01b32a1e7ddfcbf2f9df89b1062cc17ef9df8808ae0dbd4b92a740093927ce1533c5ffc1a689673578bbba6993ce60223f150aa0aa69365a76a5e1b4530eea54'
